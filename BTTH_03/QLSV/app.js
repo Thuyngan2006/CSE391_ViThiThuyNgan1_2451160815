1255ea0fbf1c605291e6050bdd5d316d19b7ea32
Thuyngan2006 <vyngan2702203@gmail.com>
2026-05-23 14:22:08 +0700

    add giai doan 3

--- BTTH_03/QLSV/app.js
+++ BTTH_03/QLSV/app.js
@@ -30,4 +30,74 @@ closeModalBtn.addEventListener("click", function(){
     studentModal.style.display = "none";
 
 });
+/* =========================
+    INPUT
+========================= */
 
+const studentId = document.getElementById("studentId");
+
+const fullName = document.getElementById("fullName");
+
+const birthDate = document.getElementById("birthDate");
+
+const className = document.getElementById("className");
+
+const score = document.getElementById("score");
+
+const email = document.getElementById("email");
+
+/* =========================
+    RESET FORM
+========================= */
+
+function resetForm(){
+
+    studentForm.reset();
+
+}
+
+/* =========================
+    NÚT HỦY
+========================= */
+
+cancelBtn.addEventListener("click", function(){
+
+    resetForm();
+
+    studentModal.style.display = "none";
+
+});
+
+/* =========================
+    SUBMIT FORM
+========================= */
+
+studentForm.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const student = {
+
+        id: studentId.value,
+
+        name: fullName.value,
+
+        birth: birthDate.value,
+
+        className: className.value,
+
+        score: score.value,
+
+        email: email.value
+
+    };
+
+    console.log(student);
+
+    message.innerText = "Lấy dữ liệu thành công";
+
+    resetForm();
+
+    studentModal.style.display = "none";
+
+});
